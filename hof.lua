167d0f33e08cbc8897cc7ac168cb944f8142e662
Yabastar <138197982+Yabastar@users.noreply.github.com>
2024-02-10 18:13:52 -0800

    Update hof.lua

--- hof.lua
+++ hof.lua
@@ -3,5 +3,5 @@ local bluelist = {1,1,1,2,2,1,2,2,4,1,4,2,5,1,5,2}
 term.setTextColor(colors.blue)
 for i=1,#bluelist,2 do
     term.setCursorPos(bluelist[i],bluelist[i+1])
-    term.write(" ")
+    term.write("a")
 end
